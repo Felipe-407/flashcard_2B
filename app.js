a3eb26d50ebbe8e73a84d4ffd62915dab3ad65a7
Felipe-407 <ribeiro.andrade.felipe@escola.pr.gov.br>
2025-10-17 10:56:43 -0300

    .

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ cartao.innerHTML = `
                 <p>${resposta}</p>
             </div>
           </div>    
-          `      
+          `
+   let respostaEstaVisivel = false;
+    respostaEstaVisivel = !respostaEstaVisivel 
+    cartao.classList.toggle('active', respostaEstaVisivel)
+   }
+   cartao.addEventListener('click', viraCartao)
+
     container.appendChild(cartao)
 }
